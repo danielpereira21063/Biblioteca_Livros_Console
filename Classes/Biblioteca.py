fa6a30e502d6a7e18396c2598a07dfb7ae991bda
Daniel Sanches <danielsanches6301@gmail.com>
2024-05-02 22:24:50 -0300

    finalização

--- Classes/Biblioteca.py
+++ Classes/Biblioteca.py
@@ -1,6 +1,6 @@
-import csv
-from Classes.Livro import Livro
-from Classes.Usuario import Usuario
+import csv  # Importa a bibioteca csv para trabalhar com arquivos CSV
+from Classes.Livro import Livro  # Importa a classe Livro do módulo Livro
+from Classes.Usuario import Usuario  # Importa a classe Usuario do módulo Usuario
 class Biblioteca:
     def __init__(self):
         self.livros = {}
@@ -27,64 +27,101 @@ class Biblioteca:
         self.salvar_livros_csv("Database/livros.csv")
     
     def cadastrar_usuario(self, nome, contato):
+        # Obtém o último ID de usuário cadastrado
         ultimo_id_usuario = self.carregar_ultimo_id_usuario_cadastrado()
+        # Incrementa o ID para o próximo usuário
         id_usuario = ultimo_id_usuario + 1
 
+        # Cria um novo usuário com as informações fornecidas
         self.usuarios[id_usuario] = Usuario(nome, id_usuario, contato)
+        # Imprime uma mensagem de sucesso com o nome do usuário e seu ID
         print(f"Usuário '{nome}' cadastrado com sucesso. ID: {id_usuario}")
+        # Salva os usuários em um arquivo CSV após cadastrar um novo usuário
         self.salvar_usuarios_csv("Database/usuarios.csv")
-    
+
     def consultar_usuarios(self, id_usuario=None):
+        # Verifica se o ID do usuário foi fornecido
         if id_usuario is None or id_usuario == "":
+            # Se nenhum ID for fornecido, lista todos os usuários cadastrados
             if self.usuarios:
+                # Imprime a lista de todos os usuários cadastrados na biblioteca
                 print("Lista de todos os usuários cadastrados na biblioteca:")
+                # Itera sobre cada usuário no dicionário de usuários
                 for id_usuario, usuario in self.usuarios.items():
+                    # Imprime as informações do usuário
                     print(f"Informações do usuário (ID {id_usuario}):")
                     print(f"Nome: {usuario.nome}")
                     print(f"Contato: {usuario.contato}")
                     print()
             else:
+                # Se não houver usuários cadastrados, imprime uma mensagem
                 print("Não há usuários cadastrados na biblioteca.")
         else:
+            # Se um ID de usuário foi fornecido, verifica se ele existe
             if id_usuario in self.usuarios:
+                # Se o ID de usuário existe, obtém as informações do usuário correspondente
                 usuario = self.usuarios[id_usuario]
+                # Imprime as informações do usuário
                 print(f"Informações do usuário (ID {id_usuario}):")
                 print(f"Nome: {usuario.nome}")
                 print(f"Contato: {usuario.contato}")
             else:
+                # Se o ID de usuário não existir, imprime uma mensagem
                 print(f"Usuário com ID {id_usuario} não encontrado.")
 
     def carregar_ultimo_id_usuario_cadastrado(self):
+            # Verifica se existem usuários cadastrados para determinar o último ID
             if self.usuarios:
+                # Se houver usuários cadastrados, cria uma lista dos IDs de usuários
                 lista_ids_usuarios = list(self.usuarios.keys())
+                # Retorna o maior ID de usuário na lista
                 return max(lista_ids_usuarios)
             else:
+                # Se não houver usuários cadastrados, retorna 0
                 return 0 
-    
+
     def carregar_ultimo_id_livro_cadastrado(self):
+            # Verifica se existem livros cadastrados para determinar o último ID
             if self.livros:
+                # Se houver livros cadastrados, cria uma lista dos IDs de livros
                 lista_ids_livros = list(self.livros.keys())
+                # Retorna o maior ID de livro na lista
                 return max(lista_ids_livros)
             else:
+                # Se não houver livros cadastrados, retorna 0
                 return 0 
-            
+
     def obter_usuario_por_id(self, id_usuario):
+        # Inicializa a variável 'usuario' como None
         usuario = None
+        # Converte o ID do usuário para um número inteiro
         id_usuario = int(id_usuario)
+        # Itera sobre cada item no dicionário de usuários
         for i in self.usuarios:
+            # Verifica se o ID do usuário atual corresponde ao ID fornecido
             if(self.usuarios[i].id_usuario == id_usuario):
+                # Se encontrar o usuário correspondente, atribui-o à variável 'usuario'
                 usuario = self.usuarios[i]
+                # Interrompe o loop
                 break
+        # Retorna o usuário encontrado (ou None se não for encontrado)
         return usuario
-            
+
     def emprestar_livro(self, livro_escolhido, id_usuario):
+        # Inicializa a variável 'disponivel' como False
         disponivel = False
+        # Inicializa o índice do livro como -1
         idxLivro = -1
+        # Itera sobre cada índice no dicionário de livros
         for idx in self.livros:
+            # Obtém o livro correspondente ao índice atual
             livro_encontrado = self.livros[idx]
+            # Verifica se o livro atual é o mesmo que o livro escolhido e se ainda há cópias disponíveis
             if (livro_encontrado == livro_escolhido and livro_escolhido.copias > 0):
+                # Se o livro estiver disponível, define 'disponivel' como True e armazena o índice do livro
                 disponivel = True
                 idxLivro = idx
+                # Interrompe o loop
                 break
 
 
@@ -227,7 +264,7 @@ class Biblioteca:
         # Abrir o arquivo CSV para escrita
         with open(nome_arquivo, 'w', newline='') as arquivo_csv:
             # Definir os nomes das colunas
-            colunas = ['ID', 'Título', 'Autor', 'Ano', 'Cópias']
+            colunas = ['ID', 'Titulo', 'Autor', 'Ano', 'Copias']
             # Criar o escritor CSV
             escritor_csv = csv.DictWriter(arquivo_csv, fieldnames=colunas)
             # Escrever o cabeçalho
@@ -236,10 +273,10 @@ class Biblioteca:
             for id_livro, livro in self.livros.items():
                 escritor_csv.writerow({
                     'ID': id_livro,
-                    'Título': livro.titulo,
+                    'Titulo': livro.titulo,
                     'Autor': livro.autor,
                     'Ano': livro.ano_publicacao,
-                    'Cópias': livro.copias
+                    'Copias': livro.copias
                 })
 
     def salvar_usuarios_csv(self, nome_arquivo):
@@ -260,29 +297,43 @@ class Biblioteca:
                 })
 
     def carregar_livros_csv(self, nome_arquivo):
-            try:
-                with open(nome_arquivo, 'r', newline='') as arquivo_csv:
-                    leitor_csv = csv.DictReader(arquivo_csv)
-                    for linha in leitor_csv:
-                        id_livro = int(linha['ID'])
-                        titulo = linha['Título']
-                        autor = linha['Autor']
-                        ano = int(linha['Ano'])
-                        copias = int(linha['Cópias'])
-                        self.livros[id_livro] = Livro(id_livro, titulo, autor, ano, copias)
-            except FileNotFoundError:
-                print(f"Arquivo '{nome_arquivo}' não encontrado. Nenhum livro carregado.")
-
-    def carregar_usuarios_csv(self, nome_arquivo):
+        # Tentar abrir o arquivo CSV para leitura
         try:
             with open(nome_arquivo, 'r', newline='') as arquivo_csv:
+                # Criar um leitor CSV para ler o arquivo linha por linha
                 leitor_csv = csv.DictReader(arquivo_csv)
+                # Iterar sobre cada linha do arquivo CSV
                 for linha in leitor_csv:
+                    # Extrair informações de cada linha
+                    id_livro = int(linha['ID'])
+                    titulo = linha['Titulo']
+                    autor = linha['Autor']
+                    ano = int(linha['Ano'])
+                    copias = int(linha['Copias'])
+                    # Adicionar o livro ao dicionário de livros da biblioteca
+                    self.livros[id_livro] = Livro(id_livro, titulo, autor, ano, copias)
+        # Lidar com a exceção se o arquivo não for encontrado
+        except FileNotFoundError:
+            # Imprimir uma mensagem informando que o arquivo não foi encontrado
+            print(f"Arquivo '{nome_arquivo}' não encontrado. Nenhum livro carregado.")
+
+    def carregar_usuarios_csv(self, nome_arquivo):
+        # Tentar abrir o arquivo CSV para leitura
+        try:
+            with open(nome_arquivo, 'r', newline='') as arquivo_csv:
+                # Criar um leitor CSV para ler o arquivo linha por linha
+                leitor_csv = csv.DictReader(arquivo_csv)
+                # Iterar sobre cada linha do arquivo CSV
+                for linha in leitor_csv:
+                    # Extrair informações de cada linha
                     id_usuario = int(linha['ID'])
                     nome = linha['Nome']
                     contato = linha['Contato']
+                    # Adicionar o usuário ao dicionário de usuários da biblioteca
                     self.usuarios[id_usuario] = Usuario(nome, id_usuario, contato)
+        # Lidar com a exceção se o arquivo não for encontrado
         except FileNotFoundError:
+            # Imprimir uma mensagem informando que o arquivo não foi encontrado
             print(f"Arquivo '{nome_arquivo}' não encontrado. Nenhum usuário carregado.")
 
     def salvar_livros_emprestados_csv(self, nome_arquivo):
@@ -318,5 +369,3 @@ class Biblioteca:
                         self.livros_emprestados[id_usuario].append(id_livro)
         except FileNotFoundError:
             print(f"Arquivo '{nome_arquivo}' não encontrado. Nenhum livro emprestado carregado.")
-
-
--- __main__.py
+++ __main__.py
@@ -24,88 +24,19 @@ def abrirMenu():
         opcao = input("Escolha uma opção: ")
 
         if opcao == '1':
-            titulo = input("Título do Livro: ").strip()
-            autor = input("Autor do Livro: ").strip()
-            if not titulo or not autor:
-                print("###### Título e autor não podem ser vazios. ######")
-                continue
-            try:
-                ano = int(input("Ano de Publicação: "))
-                copias = int(input("Número de Cópias: "))
-                if ano <= 0 or copias < 0:
-                    raise ValueError("###### Ano de publicação e número de cópias devem ser valores maiores que 0. ######")
-            except ValueError as e:
-                if "invalid literal for int()" in str(e):
-                    print("###### Ano de publicação e número de cópias devem ser valores maiores que 0. ######")
-                else:
-                    print(f"###### {str(e)} ######")
-                continue  # Retornar ao início do loop
-
-            biblioteca.cadastrar_livro(titulo, autor, ano, copias)
+            cadastrar_livro(biblioteca)
         elif opcao == '2':
-            busca = input("Informe o título, autor ou ano para consulta (enter para consulta completa): ")
-            biblioteca.consultar_livros(busca)
+            consultar_livros(biblioteca)
         elif opcao == '3':
-            livros_emprestados = biblioteca.get_lista_livros_emprestados()
-            #LISTAR AQYUUQYQUQU
-
-            if not livros_emprestados:
-                print("Não há livros emprestados.")
-            else:
-                print("Lista de Livros emprestados: ")
-                for i, livro in enumerate(livros_emprestados):
-                    print(f"{i + 1}. Usuário: {livro['nome_usuario']} Título: {livro['titulo']}, Autor: {livro['autor']}, Ano: {livro['ano_publicacao']}, Cópias: {livro['copias']}")
-
-                opcao = input("Digite o número do livro que deseja devolver (ou digite '0' para sair): ")
-                if opcao.isdigit():
-                    opcao = int(opcao)
-                    if opcao == 0:
-                        print("Operação cancelada.")
-                    elif opcao > 0 and opcao <= len(livros_emprestados):
-                        livro = livros_emprestados[opcao - 1]
-                        print(f"Você escolheu devolver o livro '{livro['titulo']}'.")
-                        biblioteca.devolver_livro(livro['titulo'], livro['idUsuario'])
-                    else:
-                        print("Escolha inválida. Por favor, escolha um número válido.")
-                else:
-                    print("Escolha inválida. Por favor, digite um número.")
-
-
+            devolver_livro(biblioteca)
         elif opcao == '4':
-            livros_disponiveis = biblioteca.get_lista_livros()
-            if not livros_disponiveis:
-                print("Não há livros disponíveis para empréstimo.")
-            else:
-                print("Lista de Livros Disponíveis para Empréstimo: \n")
-                for i, livro in enumerate(livros_disponiveis):
-                    print(f"{i + 1}. Título: {livro.titulo}, Autor: {livro.autor}, Ano: {livro.ano_publicacao}, Cópias Disponíveis: {livro.copias}")
-
-            opcao = input("\nDigite o número do livro que deseja emprestar ('0' para sair): ")
-
-            Biblioteca.listar_detalhes_usuarios(self=biblioteca)
-
-            if opcao.isdigit():
-                opcao = int(opcao)
-                if opcao == 0:
-                    print("Operação cancelada.")
-                elif opcao > 0 and opcao <= len(livros_disponiveis):
-                    livro_escolhido = livros_disponiveis[opcao - 1]
-                    id_usuario = input("ID do Usuário que está Emprestando: ")
-                    biblioteca.emprestar_livro(livro_escolhido, id_usuario)
-                else:
-                    print("Escolha inválida. Por favor, escolha um número válido.")
-            else:
-                print("Escolha inválida. Por favor, digite um número.")
-                
+            emprestar_livro(biblioteca)
         elif opcao == '5':
-            nome = input("Nome do Usuário: ")
-            contato = input("Contato: ")
-            biblioteca.cadastrar_usuario(nome, contato)
+            cadastrar_usuario(biblioteca)
         elif opcao == '6':
-            id_usuario = input("ID do Usuário para Consulta (vazio caso queira listar todos): \n")
-            biblioteca.consultar_usuarios(id_usuario)
+            consultar_usuarios(biblioteca)
         elif opcao == '7':
-            biblioteca.gerar_relatorios()
+            gerar_relatorios(biblioteca)
         elif opcao == '0':
             print("Saindo do sistema...")
             break
@@ -114,6 +45,111 @@ def abrirMenu():
         
         input("Pressione Enter para continuar...")
         limpar_console()
+
+def cadastrar_livro(biblioteca):
+    print("\nCadastrar Livros")
+    print("===============================")
+    titulo = input("Título do Livro: ").strip()
+    autor = input("Autor do Livro: ").strip()
+    if not titulo or not autor:
+        print("###### Título e autor não podem ser vazios. ######")
+        return
+    try:
+        ano = int(input("Ano de Publicação: "))
+        copias = int(input("Número de Cópias: "))
+        if ano <= 0 or copias < 0:
+            raise ValueError("###### Ano de publicação e número de cópias devem ser valores maiores que 0. ######")
+    except ValueError as e:
+        if "invalid literal for int()" in str(e):
+            print("###### Ano de publicação e número de cópias devem ser valores maiores que 0. ######")
+        else:
+            print(f"###### {str(e)} ######")
+        return  # Retornar ao início do loop
+
+    biblioteca.cadastrar_livro(titulo, autor, ano, copias)
+
+def consultar_livros(biblioteca):
+    print("\nConsultar Livros")
+    print("===============================")
+    busca = input("Informe o título, autor ou ano para consulta (pressione Enter para consulta completa): ")
+    biblioteca.consultar_livros(busca)
+
+def devolver_livro(biblioteca):
+    print("\nDevolução de Livro")
+    print("===============================")
+    livros_emprestados = biblioteca.get_lista_livros_emprestados()
+    if not livros_emprestados:
+        print("Não há livros emprestados.")
+    else:
+        print("Lista de Livros emprestados: ")
+        for i, livro in enumerate(livros_emprestados):
+            print(f"{i + 1}. Usuário: {livro['nome_usuario']} Título: {livro['titulo']}, Autor: {livro['autor']}, Ano: {livro['ano_publicacao']}, Cópias: {livro['copias']}")
+
+        opcao = input("Digite o número do livro que deseja devolver (ou digite '0' para sair): ")
+        if opcao.isdigit():
+            opcao = int(opcao)
+            if opcao == 0:
+                print("Operação cancelada.")
+            elif opcao > 0 and opcao <= len(livros_emprestados):
+                livro = livros_emprestados[opcao - 1]
+                print(f"Você escolheu devolver o livro '{livro['titulo']}'.")
+                biblioteca.devolver_livro(livro['titulo'], livro['idUsuario'])
+            else:
+                print("Escolha inválida. Por favor, escolha um número válido.")
+        else:
+            print("Escolha inválida. Por favor, digite um número.")
+
+def emprestar_livro(biblioteca):
+    print("\nEmpréstimo de Livro")
+    print("===============================")
+    livros_disponiveis = biblioteca.get_lista_livros()
+    if not livros_disponiveis:
+        print("Não há livros disponíveis para empréstimo.")
+    else:
+        print("Lista de Livros Disponíveis para Empréstimo: \n")
+        for i, livro in enumerate(livros_disponiveis):
+            print(f"{i + 1}. Título: {livro.titulo}, Autor: {livro.autor}, Ano: {livro.ano_publicacao}, Cópias Disponíveis: {livro.copias}")
+
+    opcao = input("\nDigite o número do livro que deseja emprestar ('0' para sair): ")
+    if opcao.isdigit():
+        opcao = int(opcao)
+        if opcao == 0:
+            print("Operação cancelada.")
+        elif opcao > 0 and opcao <= len(livros_disponiveis):
+            livro_escolhido = livros_disponiveis[opcao - 1]
+            id_usuario = input("ID do Usuário que está Emprestando: ")
+            biblioteca.emprestar_livro(livro_escolhido, id_usuario)
+        else:
+            print("Escolha inválida. Por favor, escolha um número válido.")
+    else:
+        print("Escolha inválida. Por favor, digite um número.")
+
+def cadastrar_usuario(biblioteca):
+    print("\nCadastrar Usuários")
+    print("===============================")
+    nome = input("Nome do Usuário: ")  # Pergunta pelo nome do usuário
+    contato = input("Contato: ")  # Pede o contato
+    if nome.strip() == "" or contato.strip() == "":  # Se nome ou contato estiverem vazios
+        print("Nome e contato não podem estar vazios. Por favor, tente novamente.")  # Indica que nome e contato não podem ser vazios
+    else:
+        biblioteca.cadastrar_usuario(nome, contato)  # Chama o método de cadastro na biblioteca
+
+
+def consultar_usuarios(biblioteca):
+    print("\nConsultar Usuários")
+    print("===============================")
+    id_usuario = input("ID do Usuário para Consulta (vazio caso queira listar todos): \n")
+    biblioteca.consultar_usuarios(id_usuario)
+
+def gerar_relatorios(biblioteca):
+    print("\nRelatórios")
+    print("===============================")
+    print("Relatório de Livros:")
+    biblioteca.listar_detalhes_livros()
+    print("\nRelatório de Usuários:")
+    biblioteca.listar_detalhes_usuarios()
+    print("\nRelatório de Livros Emprestados:")
+    biblioteca.listar_detalhes_livros_emprestados()
 
 if __name__ == "__main__":
     abrirMenu()
